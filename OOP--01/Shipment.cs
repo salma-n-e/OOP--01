9c591aa797e912d021a4655b158c3bb22ea33553
سلمى نجم الدين محمد حسن بيومى العوفى stu <425254647@sha.edu.eg>
2026-09-09 20:44:06 +0300

    Shipment struct

--- OOP--01/Shipment.cs
+++ OOP--01/Shipment.cs
@@ -4,7 +4,90 @@ using System.Text;
 
 namespace SmartDelivery
 {
-    internal class Shipment
-    {
+  
+        // Shipment struct
+        public struct Shipment
+        {
+            private string TrackingCode;
+            private string Description;
+            private double Weight;
+            private Decimal DeliveryFee;
+            public Models Destination { get; set; }
+            public string trackingCode
+            {
+                get { return TrackingCode; }
+
+            }
+            public string description
+            {
+                get
+                {
+                    return Description;
+                }
+                set
+                {
+                    if (!string.IsNullOrWhiteSpace(value))
+                        Description = value;
+                }
+
+            }
+            public double weight
+            {
+                get { return Weight; }
+                set
+                {
+                    if (value > 0)
+                        Weight = value;
+                }
+            }
+            public decimal deliveryFee
+            {
+                get { return DeliveryFee; }
+                set
+                {
+                    if (value >= 0)
+                        DeliveryFee = value;
+                }
+            }
+            public decimal EstimatedDeliveryTCost
+            {
+                get { return DeliveryFee + ((decimal)Weight * 5); }
+
+            }
+            public Shipment(string TrackingCode)
+            {
+
+                this.TrackingCode = string.IsNullOrWhiteSpace(TrackingCode) ? "Unknown" : TrackingCode;
+                Description = "Unknown";
+                Weight = 1;
+                DeliveryFee = 50;
+                Destination = new Models("Unknown", "Unknown", 0);
+
+            }
+            public Shipment(string TrackingCode, string Description, double Weight, decimal DeliveryFee, Models Destination)
+            {
+                this.TrackingCode = string.IsNullOrWhiteSpace(TrackingCode) ? "Unknown" : TrackingCode;
+                this.Description = string.IsNullOrWhiteSpace(Description) ? "Unknown" : Description;
+                this.Weight = Weight > 0 ? Weight : 1;
+                this.DeliveryFee = DeliveryFee >= 0 ? DeliveryFee : 50;
+                this.Destination = Destination;
+            }
+            public void UpdateDeliveryFee(decimal updatedFee)
+            {
+                if (updatedFee >= 0)
+                    DeliveryFee = updatedFee;
+            }
+            public void printShipmentDetails()
+            {
+                Console.WriteLine($"Tracking Code: {TrackingCode}");
+                Console.WriteLine($"Description: {Description}");
+                Console.WriteLine($"Weight: {Weight} kg");
+                Console.WriteLine($"Delivery Fee: ${DeliveryFee}");
+                Console.WriteLine($"Estimated Total Cost: ${EstimatedDeliveryTCost}");
+                Console.WriteLine($"Destination: {Destination.GetFullAddress()}");
+            }
+
+        }
+        
     }
-}
+
